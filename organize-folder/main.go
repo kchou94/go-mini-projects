b6beb2189ba7cb8892ab7806d30a89915a565d72
Berny Linville <bernylinville@devopsthink.org>
2021-12-06 17:28:43 +0800

    update organize-folder

--- organize-folder/main.go
+++ organize-folder/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 // Check for any error
@@ -15,10 +16,72 @@ func check(err error) {
 }
 
 // function to create default folders such as Images, Music, Docs, Others, Videos
-func createDefaultFolders(targetFolder string) {}
+func createDefaultFolders(targetFolder string) {
+	defaultFolders := []string{"Docs", "Images", "Music", "Videos", "Others"}
+
+	for _, folder := range defaultFolders {
+		_, err := os.Stat(filepath.Join(targetFolder, folder))
+		if os.IsNotExist(err) {
+			os.Mkdir(filepath.Join(targetFolder, folder), 0755)
+		}
+	}
+}
 
 // function to Organize folders
-func organizeFolders(targetFolder string) {}
+func organizeFolders(targetFolder string) {
+	filesAndFolders, err := os.ReadDir(targetFolder)
+	check(err)
+
+	// to track how many files moved
+	noOfFiles := 0
+
+	for _, fileAndFolder := range filesAndFolders {
+		// Check for files
+		if !fileAndFolder.IsDir() {
+			fileInfo, err := fileAndFolder.Info()
+			check(err)
+
+			// Get the file full path
+			oldPath := filepath.Join(targetFolder, fileInfo.Name())
+			fileExt := filepath.Ext(oldPath)
+
+			switch fileExt {
+			case ".jpg", ".jpeg", ".png", ".gif", ".bmp":
+				newPath := filepath.Join(targetFolder, "Images", fileInfo.Name())
+				err = os.Rename(oldPath, newPath)
+				check(err)
+				noOfFiles++
+			case ".mp3", ".wav", ".flac", ".aac", ".ogg":
+				newPath := filepath.Join(targetFolder, "Music", fileInfo.Name())
+				err = os.Rename(oldPath, newPath)
+				check(err)
+				noOfFiles++
+			case ".doc", ".docx", ".txt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx":
+				newPath := filepath.Join(targetFolder, "Docs", fileInfo.Name())
+				err = os.Rename(oldPath, newPath)
+				check(err)
+				noOfFiles++
+			case ".mp4", ".mkv", ".avi", ".flv", ".mov", ".wmv", ".3gp", ".m4v":
+				newPath := filepath.Join(targetFolder, "Videos", fileInfo.Name())
+				err = os.Rename(oldPath, newPath)
+				check(err)
+				noOfFiles++
+			default:
+				newPath := filepath.Join(targetFolder, "Others", fileInfo.Name())
+				err = os.Rename(oldPath, newPath)
+				check(err)
+				noOfFiles++
+			}
+		}
+	}
+
+	// Print how many files moved
+	if noOfFiles > 0 {
+		fmt.Printf("%v number of files moved \n", noOfFiles)
+	} else {
+		fmt.Println("No files moved")
+	}
+}
 
 func main() {
 	// Get the user input - target folder needs to be organized
